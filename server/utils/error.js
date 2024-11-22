30cc60dc9ed207fc1a2c4ca57787218669bff542
kudo07 <vknwl077@gmail.com>
2024-11-22 07:02:11 +0530

    vv

--- server/utils/error.js
+++ server/utils/error.js
@@ -1,8 +1,6 @@
-export const errorHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err); // Prevent duplicate responses
-  }
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({ success: false, message });
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
 };
